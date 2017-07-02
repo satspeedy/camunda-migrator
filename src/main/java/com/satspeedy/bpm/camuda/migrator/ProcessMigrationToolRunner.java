c77c2dd295e06903f2b015fe06da262e15c8ae0c
satspeedy <satspeedy@hotmail.com>
2017-07-02 17:43:16 +0200

    removed unused cli commands

--- src/main/java/com/satspeedy/bpm/camuda/migrator/ProcessMigrationToolRunner.java
+++ src/main/java/com/satspeedy/bpm/camuda/migrator/ProcessMigrationToolRunner.java
@@ -18,8 +18,6 @@ public class ProcessMigrationToolRunner implements CommandLineRunner {
     private static final Logger LOG = LoggerFactory.getLogger(ProcessMigrationToolRunner.class);
 
     private static final String COMMAND_MIGRATE = "migrate";
-    private static final String COMMAND_INFO = "info";
-    private static final String COMMAND_HELP = "help";
     private static final String COMMAND_EXIT = "exit";
 
     @Autowired
@@ -55,7 +53,7 @@ public class ProcessMigrationToolRunner implements CommandLineRunner {
         try (Scanner scanner = new Scanner(System.in)) {
             String text;
             do {
-                LOG.info(String.format("%nType %s, %s, %s or %s as a command:", COMMAND_MIGRATE, COMMAND_INFO, COMMAND_HELP, COMMAND_EXIT));
+                LOG.info(String.format("%nType %s or %s as a command:", COMMAND_MIGRATE, COMMAND_EXIT));
                 text = scanner.nextLine();
                 text = performCommand(text);
             } while (!text.equals(COMMAND_EXIT));
@@ -67,11 +65,6 @@ public class ProcessMigrationToolRunner implements CommandLineRunner {
         switch (text) {
             case COMMAND_MIGRATE:
                 migrate();
-                break;
-            case COMMAND_INFO:
-                LOG.info(String.format("Which kind of info do you need?", text));
-                break;
-            case COMMAND_HELP:
                 break;
             case COMMAND_EXIT:
                 break;
